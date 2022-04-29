2f43901dec7b586f7077ffcb476a4f4c1fc943ac
uch2ha <sinyavskiy.d@gmail.com>
2022-04-29 13:48:35 +0300

    Created Game component

--- App.js
+++ App.js
@@ -1,150 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
-import { colors, CLEAR, ENTER } from "./src/tools";
-import Keyboard from "./src/components/Keyboard/Keyboard";
+import { Header } from "react-native-elements";
+import { Icon } from "react-native-elements";
+import { colors } from "./src/tools";
 import { useState, useEffect } from "react";
-
-const number_of_tries = 6;
+import Game from "./src/components/Game";
 
 export default function App() {
-   const word = "hello";
-   const letters = word.split("");
-
-   const [rows, setRows] = useState(
-      new Array(number_of_tries).fill(new Array(letters.length).fill(""))
-   );
-   const [currentRow, setCurrentRow] = useState(0);
-   const [currentColumn, setCurrentColumn] = useState(0);
-   const [gameState, setGameState] = useState("playing"); //win, lose, playing
-
-   useEffect(() => {
-      if (currentRow > 0) {
-         checkGameState();
-      }
-   }, [currentRow]);
-
-   const checkGameState = () => {
-      if (checkIfWin()) {
-         setGameState("win");
-      } else if (checkIfLose()) {
-         setGameState("lose");
-      }
-   };
-
-   const checkIfWin = () => {
-      const row = rows[currentRow - 1];
-      return row.every((letter, i) => letter === letters[i]);
-   };
-
-   const checkIfLose = () => {
-      return currentRow === rows.length;
-   };
-
-   const copyArray = (arr) => {
-      return [...arr.map((rows) => [...rows])];
-   };
-
-   const onKeyPressed = (key) => {
-      if (gameState !== "playing") {
-         return;
-      }
-
-      const updatedArr = copyArray(rows);
-
-      if (key === CLEAR) {
-         const prevCol = currentColumn - 1;
-         if (prevCol >= 0) {
-            updatedArr[currentRow][prevCol] = "";
-            setRows(updatedArr);
-            setCurrentColumn(prevCol);
-         }
-         return;
-      }
-
-      if (key === ENTER) {
-         if (currentColumn === rows[0].length) {
-            setCurrentRow(currentRow + 1);
-            setCurrentColumn(0);
-         }
-
-         return;
-      }
-
-      if (currentColumn < rows[0].length) {
-         updatedArr[currentRow][currentColumn] = key;
-         setRows(updatedArr);
-         setCurrentColumn(currentColumn + 1);
-      }
-   };
-
-   const isCellActive = (row, col) => {
-      return row === currentRow && col === currentColumn;
-   };
-
-   const getCellBGcolor = (row, col) => {
-      const letter = rows[row][col];
-
-      if (row >= currentRow) {
-         return colors.black;
-      }
-
-      if (letter === letters[col]) {
-         return colors.primary;
-      }
-      if (letters.includes(letter)) {
-         return colors.secondary;
-      }
-      return colors.darkgrey;
-   };
-
-   const getAllLettersWithColor = (color) => {
-      return rows.flatMap((row, i) =>
-         row.filter((cell, j) => getCellBGcolor(i, j) === color)
-      );
-   };
-
-   const greenCaps = getAllLettersWithColor(colors.primary);
-   const yellowCaps = getAllLettersWithColor(colors.secondary);
-   const greyCaps = getAllLettersWithColor(colors.darkgrey);
-
    return (
       <SafeAreaView style={styles.container}>
-         <StatusBar style="light" />
-         <View>
+         {/* <StatusBar style="light" /> */}
+         <Header
+            // statusBarProps={{ barStyle: "light-content" }}
+            // barStyle="light-content"
+            backgroundColor="black"
+            containerStyle={{}}
+            leftComponent={{
+               icon: "menu",
+               color: "white",
+            }}
+            centerComponent={<Text style={styles.title}>WORDLE</Text>}
+            rightComponent={{ icon: "home", color: "#eee" }}
+         />
+         {/* <View>
             <Text style={styles.title}>WORDLE</Text>
-         </View>
-         <ScrollView style={styles.map}>
-            {rows.map((row, i) => (
-               <View key={`row-${i}`} style={styles.row}>
-                  {row.map((letter, j) => (
-                     <View
-                        key={`cell-${i}-${j}`}
-                        style={[
-                           styles.cell,
-                           {
-                              borderColor: isCellActive(i, j)
-                                 ? colors.lightgrey
-                                 : colors.darkgrey,
-                              backgroundColor: getCellBGcolor(i, j),
-                           },
-                        ]}
-                     >
-                        <Text style={styles.cellText}>
-                           {letter.toUpperCase()}
-                        </Text>
-                     </View>
-                  ))}
-               </View>
-            ))}
-         </ScrollView>
-         <View>
-            <Keyboard
-               onKeyPressed={onKeyPressed}
-               greenCaps={greenCaps}
-               yellowCaps={yellowCaps}
-               greyCaps={greyCaps}
-            />
-         </View>
+         </View> */}
+         <Game />
       </SafeAreaView>
    );
 }
@@ -154,37 +35,12 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: colors.black,
       alignItems: "center",
-      paddingTop: Platform.OS === "android" ? 40 : 0,
+      paddingTop: Platform.OS === "android" ? 20 : 0,
    },
    title: {
       color: colors.lightgrey,
       fontSize: 32,
       fontWeight: "bold",
       letterSpacing: 5,
-   },
-   map: {
-      marginVertical: 15,
-      alignSelf: "stretch",
-   },
-   row: {
-      alignSelf: "stretch",
-      flexDirection: "row",
-      justifyContent: "center",
-   },
-   cell: {
-      flex: 1,
-      aspectRatio: 1,
-      borderWidth: 3,
-      borderColor: colors.darkgrey,
-      margin: 3,
-      maxWidth: 70,
-      justifyContent: "center",
-      alignItems: "center",
-   },
-
-   cellText: {
-      color: colors.lightgrey,
-      fontWeight: "bold",
-      fontSize: 28,
    },
 });
---
+++ src/components/Game.js
@@ -0,0 +1,185 @@
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import Keyboard from "./Keyboard/Keyboard";
+import { colors, CLEAR, ENTER } from "../tools";
+
+const number_of_tries = 6;
+
+export default function Game() {
+   const word = "hello";
+   const letters = word.split("");
+
+   const [rows, setRows] = useState(
+      new Array(number_of_tries).fill(new Array(letters.length).fill(""))
+   );
+   const [currentRow, setCurrentRow] = useState(0);
+   const [currentColumn, setCurrentColumn] = useState(0);
+   const [gameState, setGameState] = useState("playing"); //win, lose, playing
+
+   useEffect(() => {
+      if (currentRow > 0) {
+         checkGameState();
+      }
+   }, [currentRow]);
+
+   const checkGameState = () => {
+      if (checkIfWin()) {
+         setGameState("win");
+      } else if (checkIfLose()) {
+         setGameState("lose");
+      }
+   };
+
+   const checkIfWin = () => {
+      const row = rows[currentRow - 1];
+      return row.every((letter, i) => letter === letters[i]);
+   };
+
+   const checkIfLose = () => {
+      return currentRow === rows.length;
+   };
+
+   const copyArray = (arr) => {
+      return [...arr.map((rows) => [...rows])];
+   };
+
+   const onKeyPressed = (key) => {
+      if (gameState !== "playing") {
+         return;
+      }
+
+      const updatedArr = copyArray(rows);
+
+      if (key === CLEAR) {
+         const prevCol = currentColumn - 1;
+         if (prevCol >= 0) {
+            updatedArr[currentRow][prevCol] = "";
+            setRows(updatedArr);
+            setCurrentColumn(prevCol);
+         }
+         return;
+      }
+
+      if (key === ENTER) {
+         if (currentColumn === rows[0].length) {
+            setCurrentRow(currentRow + 1);
+            setCurrentColumn(0);
+         }
+
+         return;
+      }
+
+      if (currentColumn < rows[0].length) {
+         updatedArr[currentRow][currentColumn] = key;
+         setRows(updatedArr);
+         setCurrentColumn(currentColumn + 1);
+      }
+   };
+
+   const isCellActive = (row, col) => {
+      return row === currentRow && col === currentColumn;
+   };
+
+   const getCellBGcolor = (row, col) => {
+      const letter = rows[row][col];
+
+      if (row >= currentRow) {
+         return colors.black;
+      }
+
+      if (letter === letters[col]) {
+         return colors.primary;
+      }
+      if (letters.includes(letter)) {
+         return colors.secondary;
+      }
+      return colors.darkgrey;
+   };
+
+   const getAllLettersWithColor = (color) => {
+      return rows.flatMap((row, i) =>
+         row.filter((cell, j) => getCellBGcolor(i, j) === color)
+      );
+   };
+
+   const greenCaps = getAllLettersWithColor(colors.primary);
+   const yellowCaps = getAllLettersWithColor(colors.secondary);
+   const greyCaps = getAllLettersWithColor(colors.darkgrey);
+
+   return (
+      <View>
+         <ScrollView style={styles.map}>
+            {rows.map((row, i) => (
+               <View key={`row-${i}`} style={styles.row}>
+                  {row.map((letter, j) => (
+                     <View
+                        key={`cell-${i}-${j}`}
+                        style={[
+                           styles.cell,
+                           {
+                              borderColor: isCellActive(i, j)
+                                 ? colors.lightgrey
+                                 : colors.darkgrey,
+                              backgroundColor: getCellBGcolor(i, j),
+                           },
+                        ]}
+                     >
+                        <Text style={styles.cellText}>
+                           {letter.toUpperCase()}
+                        </Text>
+                     </View>
+                  ))}
+               </View>
+            ))}
+         </ScrollView>
+         <View>
+            <Keyboard
+               onKeyPressed={onKeyPressed}
+               greenCaps={greenCaps}
+               yellowCaps={yellowCaps}
+               greyCaps={greyCaps}
+            />
+         </View>
+      </View>
+   );
+}
+
+const styles = StyleSheet.create({
+   // container: {
+   //    flex: 1,
+   //    backgroundColor: colors.black,
+   //    alignItems: "center",
+   //    paddingTop: Platform.OS === "android" ? 40 : 0,
+   // },
+   // title: {
+   //    color: colors.lightgrey,
+   //    fontSize: 32,
+   //    fontWeight: "bold",
+   //    letterSpacing: 5,
+   // },
+   map: {
+      marginVertical: 15,
+      alignSelf: "stretch",
+   },
+   row: {
+      alignSelf: "stretch",
+      flexDirection: "row",
+      justifyContent: "center",
+   },
+   cell: {
+      flex: 1,
+      aspectRatio: 1,
+      borderWidth: 3,
+      borderColor: colors.darkgrey,
+      margin: 3,
+      maxWidth: 70,
+      justifyContent: "center",
+      alignItems: "center",
+   },
+
+   cellText: {
+      color: colors.lightgrey,
+      fontWeight: "bold",
+      fontSize: 28,
+   },
+});
